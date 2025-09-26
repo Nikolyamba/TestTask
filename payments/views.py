079f59549bedd6f6dc462ab5bfe16d84b0315882
Nikolyamba <niko7tkc@yandex.ru>
2025-09-26 09:01:03 +0300

    i completed my test task

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import PaymentInfo
 
-# Create your views here.
+def payments_chart(request):
+    payments = PaymentInfo.objects.select_related("payer_id").order_by("pay_date")
+    data = [
+        {
+            "date": p.pay_date.strftime("%Y-%m-%d"),
+            "amount": float(p.amount),
+            "payer": f"{p.payer_id.first_name} {p.payer_id.last_name}"
+        }
+        for p in payments
+    ]
+    return render(request, "payments/chart.html", {"data": data})
